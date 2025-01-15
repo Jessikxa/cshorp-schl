d5a9793abd77e28efbd82cc53407ca24ec09b0ca
Jessikxa <iamjessicadurkin@gmail.com>
2025-01-15 09:55:49 +0100

    15/01/25
    alles werkt, behalve de eerste naam input. daar staat geen text als de naam fout it

---
+++ NAAM CHECK TESTING.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+
+// alert waar je je naam in kan vullen 
+
+Console.WriteLine("Enter name:");
+
+string inputName = Console.ReadLine();
+string CorrectName = "jessica"; // ik wil ook dat het met hoofdletter kan werken
+
+while (inputName != CorrectName)
+{
+    Console.WriteLine("Enter name:");
+    inputName = Console.ReadLine();
+    Console.WriteLine("Name doesn't match");
+    if (inputName == CorrectName)
+    {
+        Console.WriteLine("Your name is:" + CorrectName);
+    }
+}
+
+
+
+//if (inputName == CorrectName)
+//{
+//    Console.WriteLine("Your name is:" + CorrectName);
+//}
+//else
+//{
+//    Console.WriteLine("Name doesn't match")
+//}
+    
+
+
+
+
+//how to check naaaaaaaaaaaaaaaaameee

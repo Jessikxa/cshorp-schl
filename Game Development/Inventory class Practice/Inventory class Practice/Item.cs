576926087c3184d3e241bc587514d1f98d9da1d9
Jessikxa <iamjessicadurkin@gmail.com>
2025-03-06 13:59:05 +0100

    inventory class list itemss aanmaken

---
+++ Game Development/Inventory class Practice/Inventory class Practice/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_class_Practice
+{
+    internal class Item
+    {
+        public string ItemsCollected;
+
+        public Item(String Yitem) 
+        {
+            ItemsCollected = Yitem;
+        }
+    }
+}

72232486861ba4d3ff5fa9a178e29fdd937207eb
Jessikxa <iamjessicadurkin@gmail.com>
2025-03-17 11:23:29 +0100

    singleton gemaakt

---
+++ Singleton.Test/Singleton.Test/Manager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Test
+{
+    internal class Manager
+    {
+        private static Manager instance;
+
+        public string Name;
+        private Manager() 
+        {
+
+        }
+
+        public static Manager GetInstance()
+        {
+            if (instance == null) 
+            {
+                instance = new Manager();
+            }
+            return instance;
+        } 
+    }
+}
---
+++ Singleton.Test/Singleton.Test/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = Manager.GetInstance(); // gotta add smth to manager
+            manager.Name = "Test";
+
+
+            Console.WriteLine(manager.Name);
+        }
+        
+    }
+}
